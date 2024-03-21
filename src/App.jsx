9f7f120d8a5f72928d8b264225b064f2131092fa
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-03-21 19:05:49 +0530

    feat:developing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import styles from './App.module.css'
 import dev from './assets/dev.gif'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
    <div className={styles.container}>
    <span className={styles.title}>SPARKZ'24</span>
