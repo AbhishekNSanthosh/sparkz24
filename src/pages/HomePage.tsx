8c1864410045ad3a49091028ed1d48165568ee3d
Joeljoy1237 <joeljoy1237@gmail.com>
2026-01-08 09:36:25 +0530

    feat : add loader and remove timer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+"use client";
 import About from "@/widgets/home/About";
 import Hero from "@/widgets/home/Hero";
 import Featured from "@/widgets/home/Featured";
@@ -5,10 +6,26 @@ import Featured from "@/widgets/home/Featured";
 import BandCompetition from "@/widgets/home/BandCompetition";
 import ITBPExhibition from "@/widgets/home/ITBPExhibition";
 import ISROExhibition from "@/widgets/home/ISROExhibition";
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import Preloader from "@/widgets/home/Preloader";
 
 export default function HomePage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader key="preloader" />}
+      </AnimatePresence>
       <Hero />
       <Featured />
       <About />
